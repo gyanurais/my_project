2f5235541bc3afe3247fa5cb68e27eae88a01730
gyanurais <gyanuraiy17@gmail.com>
2021-09-14 13:29:30 +0530

    save the file main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-var a=10;
-var b=20;
-console.log(a+b);
+var i;
+for(var i=1; i<=100; i++)
+{
+    if(i%3==0){
+        console.log("fizz");
+    }
+    else if(i%5==0){
+        console.log("buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
